6538263026bd7521bbf3f72e9862d31624205aad
Antony Grove <agrove86@gmail.com>
2014-08-07 01:22:09 -0500

    3rd
    Finished non-functional version. Added fadeout intro

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
 
-
+	/* Fades out intro page into the actual quiz */
 	$('.push-button').click(function(event){
 		event.preventDefault();
 		$('.intro').animate({
 			opacity:0
-		}, 1000);
-		$('.wrapper').show();
+		}, 1500, function(){
+			$('.intro').hide();
+		});
+
 	});
 
 	/* When an answer is clicked it turns color. Allows only 1 choice to be selected. */
